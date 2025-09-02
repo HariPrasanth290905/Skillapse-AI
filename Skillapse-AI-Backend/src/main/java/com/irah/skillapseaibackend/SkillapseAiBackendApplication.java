9278e76d9ce2014af813bad826a6e0bee219dce7
irah <hariprasanth4466@gmail.com>
2025-09-02 13:29:46 +0530

    service update

---
+++ Skillapse-AI-Backend/src/main/java/com/irah/skillapseaibackend/SkillapseAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.irah.skillapseaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillapseAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillapseAiBackendApplication.class, args);
+    }
+
+}
---
+++ Skillapse-AI-Backend/src/test/java/com/irah/skillapseaibackend/SkillapseAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irah.skillapseaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillapseAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
