d298231f00091554ba5a3a9796affe7c109f0f7b
irah <hariprasanth4466@gmail.com>
2025-10-14 22:16:26 +0530

    added

---
+++ Skillapse-AI-Backend/Skillapse-Skills/Skill-Service-Config/src/test/java/com/bei/skillserviceconfig/SkillServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bei.skillserviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillServiceConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
